98b93e9f49b88a8169e963b5f86b428b05703424
Yosif Khaled <yosif.khaledjm@gmail.com>
2020-05-24 23:24:20 +0200

    Updating files

---
+++ Traversy/EdgeLedger-site/js/main.js
@@ -0,0 +1,39 @@
+// Initialize and add the map
+function initMap(){
+  //your location
+  const loc = {lat: 42.361145, lng: -71.057083};
+  //centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  //the marker, positioned at location
+  const marker = new google.maps.Marker({position: loc, map: map});
+}
+
+// Adding Opacity To Navbar When Scrolling
+window.addEventListener('scroll', function(){
+  if(window.scrollY > 150){
+    document.querySelector('#navbar').style.opacity = .9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event){
+  if(this.hash !== ''){
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    console.log(this.hash);
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 50
+      },
+      800
+    );
+    console.log(event)
+  }
+});
